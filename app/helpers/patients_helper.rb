8008030d6a00f515f662c132b18383bfab9db879
Malachi Gray <graymd.cfi@gmail.com>
2015-02-02 15:44:48 -0500

    add workflow state helper

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,15 +1,17 @@
 module PatientsHelper
 
-# @WilliamHolt = "Hello" if patient.waiting_room?
-
-
-# = workflow_state_helper_method(patient)
-
-# def workflow_state_helper_method(patient)
-#   if patient.workflow_state == "waiting_room"
-#     "Patient is Waiting"
-# end
-
-
-
+def workflow_state_helper(patient)
+  if patient.workflow_state == "waiting_room"
+    "Patient is in waiting room."
+  elsif patient.workflow_state == "doctor_checkup"
+    "Patient is with the doctor."
+  elsif patient.workflow_state == "xray"
+    "Patient is in xray."
+  elsif patient.workflow_state == "surgery"
+    "Patient is in surgery."
+  elsif patient.workflow_state == "pay_bill"
+    "Patient is paying bill."
+  elsif patient.workflow_state == "left"
+    "Patient has left."
+  end
 end
